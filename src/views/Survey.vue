061ac919aff92cea89c0e11ca96bfb02010a4537
Galen Lockwood <galenlockwood@gmail.com>
2021-10-11 19:24:46 -0400

    new likert ui

--- src/views/Survey.vue
+++ src/views/Survey.vue
@@ -1,61 +1,99 @@
 <template>
   <div class="survey">
-    <div class="container">
-      <form class="form cf">
-        <section class="plan cf">
-          <h2>Choose a plan:</h2>
-          <input type="radio" name="radio1" id="free" value="1"><label class="six col" for="free">1</label>
-          <input type="radio" name="radio1" id="basic" value="2"><label class="six col" for="basic">2</label>
-          <input type="radio" name="radio1" id="premium" value="3"><label class="six col" for="premium">3</label>
-          <input type="radio" name="radio1" id="premium" value="4"><label class="six col" for="premium">4</label>
-          <input type="radio" name="radio1" id="premium" value="5"><label class="six col" for="premium">5</label>
-          <input type="radio" name="radio1" id="premium" value="6"><label class="six col" for="premium">6</label>
 
-        </section>
-      </form>
+    
+    <div class="container py-4">
+      <h2>Positive Parenting Schema Inventory (PPSI)</h2>
+
+      <label class="fw-bold">INSTRUCTIONS:</label>
+      <p>
+        Listed below are statements that you might use to describe your parents and how they treated you. Please read each statement and decide how well it describes your parents. Choose the highest rating from 1 to 6 that describes your father and your mother when you were a child and write the number in the spaces before each statement. If someone substituted as your mother or father, please rate the scale for that person. Give your first impression and move on to the next item. Do not dwell on any item. If you did not have a mother or a father, leave the appropriate column blank. 
+      </p>
+
+      <div class="card">
+        <div class="card-body">
+          <div class="row pt-1">
+            <label class="fw-bold pb-1">RATING SCALE:</label>
+            <div class="col-sm text-center">
+              <h5 class="mb-0">1</h5>
+              Completely untrue 
+            </div>
+            <div class="col-sm text-center">
+              <h5 class="mb-0">2</h5>
+              Mostly untrue 
+            </div>
+            <div class="col-sm text-center">
+              <h5 class="mb-0">3</h5>
+              Slightly more true than untrue
+            </div>
+            <div class="col-sm text-center">
+              <h5 class="mb-0">4</h5>
+              Moderately true 
+            </div>
+            <div class="col-sm text-center">
+              <h5 class="mb-0">5</h5>
+              Mostly true
+            </div>
+            <div class="col-sm text-center">
+              <h5 class="mb-0">6</h5>
+              Describes him/her perfectly
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="row mt-4">
+        <div class="col-sm">
+          <form class="form cf">
+            <section class="plan cf">
+              <h5 class="fw-bold pb-3">Had realistic expectations of him/herself</h5>
+              <input type="radio" name="radio1" id="1" value="1"><label class="six col shadow-sm rounded-pill" for="1">1</label>
+              <input type="radio" name="radio1" id="2" value="2"><label class="six col shadow-sm rounded-pill" for="2">2</label>
+              <input type="radio" name="radio1" id="3" value="3"><label class="six col shadow-sm rounded-pill" for="3">3</label>
+              <input type="radio" name="radio1" id="4" value="4"><label class="six col shadow-sm rounded-pill" for="4">4</label>
+              <input type="radio" name="radio1" id="5" value="5"><label class="six col shadow-sm rounded-pill" for="5">5</label>
+              <input type="radio" name="radio1" id="6" value="6"><label class="six col shadow-sm rounded-pill" for="6">6</label>
+            </section>
+          </form>
+        </div>
+      </div>
+      <div class="row mt-4">
+        <div class="vol-sm-12 col-md-6">
+          <p class="fw-bold">Father</p>
+          <form class="form cf">
+            <h5 class="fw-bold pb-3">Had realistic expectations of him/herself</h5>
+            <section class="plan cf px-4">
+              <input type="radio" name="radio1" id="11" value="1"><label class="six col shadow-sm rounded-pill" for="11">1</label>
+              <input type="radio" name="radio1" id="22" value="2"><label class="six col shadow-sm rounded-pill" for="22">2</label>
+              <input type="radio" name="radio1" id="33" value="3"><label class="six col shadow-sm rounded-pill" for="33">3</label>
+              <input type="radio" name="radio1" id="44" value="4"><label class="six col shadow-sm rounded-pill" for="44">4</label>
+              <input type="radio" name="radio1" id="55" value="5"><label class="six col shadow-sm rounded-pill" for="55">5</label>
+              <input type="radio" name="radio1" id="66" value="6"><label class="six col shadow-sm rounded-pill" for="66">6</label>
+            </section>
+          </form>
+        </div>
+        <div class="vol-sm-12 col-md-6">
+          <p class="fw-bold">Mother</p>
+          <form class="form cf">
+            <h5 class="fw-bold pb-3">Had realistic expectations of him/herself</h5>
+            <section class="plan cf px-4">
+              <input type="radio" name="radio1" id="111" value="1"><label class="six col shadow-sm rounded-pill" for="111">1</label>
+              <input type="radio" name="radio1" id="222" value="2"><label class="six col shadow-sm rounded-pill" for="222">2</label>
+              <input type="radio" name="radio1" id="333" value="3"><label class="six col shadow-sm rounded-pill" for="333">3</label>
+              <input type="radio" name="radio1" id="444" value="4"><label class="six col shadow-sm rounded-pill" for="444">4</label>
+              <input type="radio" name="radio1" id="555" value="5"><label class="six col shadow-sm rounded-pill" for="555">5</label>
+              <input type="radio" name="radio1" id="666" value="6"><label class="six col shadow-sm rounded-pill" for="666">6</label>
+            </section>
+          </form>
+        </div>
+      </div>
+
     </div>
+
     <div class="container py-4">
       <div class="row">
         <div class="col-sm">
           <div class="surveywrap">
-            <h2>Positive Parenting Schema Inventory (PPSI)</h2>
-
-            <label class="fw-bold">INSTRUCTIONS:</label>
-            <p>
-              Listed below are statements that you might use to describe your parents and how they treated you. Please read each statement and decide how well it describes your parents. Choose the highest rating from 1 to 6 that describes your father and your mother when you were a child and write the number in the spaces before each statement. If someone substituted as your mother or father, please rate the scale for that person. Give your first impression and move on to the next item. Do not dwell on any item. If you did not have a mother or a father, leave the appropriate column blank. 
-            </p>
-
-            <div class="card">
-              <div class="card-body">
-                <div class="row pt-1">
-                  <label class="fw-bold pb-1">RATING SCALE:</label>
-                  <div class="col-sm text-center">
-                    <h5 class="mb-0">1</h5>
-                    Completely untrue 
-                  </div>
-                  <div class="col-sm text-center">
-                    <h5 class="mb-0">2</h5>
-                    Mostly untrue 
-                  </div>
-                  <div class="col-sm text-center">
-                    <h5 class="mb-0">3</h5>
-                    Slightly more true than untrue
-                  </div>
-                  <div class="col-sm text-center">
-                    <h5 class="mb-0">4</h5>
-                    Moderately true 
-                  </div>
-                  <div class="col-sm text-center">
-                    <h5 class="mb-0">5</h5>
-                    Mostly true
-                  </div>
-                  <div class="col-sm text-center">
-                    <h5 class="mb-0">6</h5>
-                    Describes him/her perfectly
-                  </div>
-                </div>
-              </div>
-            </div>
+           
          
 
             <form action="" class="py-3">
@@ -289,12 +327,19 @@
 
           </div>
         </div>
-        <div class="col-sm-12 text-center">
-          <button type="button" class="btn btn-danger m-2">Reset</button>
-          <button type="button" class="btn btn-primary m-2">Submit</button>
-        </div>
       </div>
     </div>
+
+
+<div class="container my-4">
+  <div class="row">
+    <div class="col-sm-12 text-center">
+      <button type="button" class="btn btn-danger m-2">Reset</button>
+      <button type="button" class="btn btn-primary m-2">Submit</button>
+    </div>
+  </div>
+</div>
+
   </div>
 </template>
 
