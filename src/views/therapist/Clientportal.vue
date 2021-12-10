e55fb21c114816013a60b2bdb698b8ec1d42054e
Galen Lockwood <galenlockwood@gmail.com>
2021-12-10 11:39:27 -0500

    added manage buttons to client portla

--- src/views/therapist/Clientportal.vue
+++ src/views/therapist/Clientportal.vue
@@ -13,7 +13,20 @@
             </ul>
             <div class="row pt-4">
                 <div class="col-sm">
-                    <h4>Consent Documents</h4>
+                    <div class="row">
+                        <div class="col-sm-6 text-start">
+                            <h4>Consent Documents</h4>
+                        </div>
+                        <div class="col-sm-6 text-end">
+                            <button type="button" class="btn btn-outline-dark">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-file-earmark-plus" viewBox="0 0 16 16">
+                                    <path d="M8 6.5a.5.5 0 0 1 .5.5v1.5H10a.5.5 0 0 1 0 1H8.5V11a.5.5 0 0 1-1 0V9.5H6a.5.5 0 0 1 0-1h1.5V7a.5.5 0 0 1 .5-.5z"/>
+                                    <path d="M14 4.5V14a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2h5.5L14 4.5zm-3 0A1.5 1.5 0 0 1 9.5 3V1H4a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1V4.5h-2z"/>
+                                  </svg>
+                                Add Consent Document
+                            </button>
+                        </div>
+                    </div>
                     <table class="table" style="table-layout: fixed;">
                         <thead>
                             <tr>
@@ -187,9 +200,39 @@
                     </table>
                 </div>
             </div>
+            <div class="row py-4">
+                <div class="col-sm-12">
+                    <h4>Read Only Files</h4>
+                </div>
+                <div class="col-sm-12 pb-2">
+                    Manage documents such as PDFs to share with your clients.
+                </div>
+                <div class="col-sm-12 pb-3">
+                    <button type="button" class="btn btn-outline-dark">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-cloud-plus" viewBox="0 0 16 16">
+                            <path fill-rule="evenodd" d="M8 5.5a.5.5 0 0 1 .5.5v1.5H10a.5.5 0 0 1 0 1H8.5V10a.5.5 0 0 1-1 0V8.5H6a.5.5 0 0 1 0-1h1.5V6a.5.5 0 0 1 .5-.5z"/>
+                            <path d="M4.406 3.342A5.53 5.53 0 0 1 8 2c2.69 0 4.923 2 5.166 4.579C14.758 6.804 16 8.137 16 9.773 16 11.569 14.502 13 12.687 13H3.781C1.708 13 0 11.366 0 9.318c0-1.763 1.266-3.223 2.942-3.593.143-.863.698-1.723 1.464-2.383zm.653.757c-.757.653-1.153 1.44-1.153 2.056v.448l-.445.049C2.064 6.805 1 7.952 1 9.318 1 10.785 2.23 12 3.781 12h8.906C13.98 12 15 10.988 15 9.773c0-1.216-1.02-2.228-2.313-2.228h-.5v-.5C12.188 4.825 10.328 3 8 3a4.53 4.53 0 0 0-2.941 1.1z"/>
+                          </svg>
+                        Add Downloadable File
+                    </button> 
+                </div>
+            </div>
             <div class="row pt-4">
                 <div class="col-sm">
-                    <h4>Intake Forms & Questionnaires</h4>
+                    <div class="row">
+                        <div class="col-sm-6 text-start">
+                            <h4>Intake Forms & Questionnaires</h4>
+                        </div>
+                        <div class="col-sm-6 text-end">
+                            <button type="button" class="btn btn-outline-dark">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-folder2" viewBox="0 0 16 16">
+                                    <path d="M1 3.5A1.5 1.5 0 0 1 2.5 2h2.764c.958 0 1.76.56 2.311 1.184C7.985 3.648 8.48 4 9 4h4.5A1.5 1.5 0 0 1 15 5.5v7a1.5 1.5 0 0 1-1.5 1.5h-11A1.5 1.5 0 0 1 1 12.5v-9zM2.5 3a.5.5 0 0 0-.5.5V6h12v-.5a.5.5 0 0 0-.5-.5H9c-.964 0-1.71-.629-2.174-1.154C6.374 3.334 5.82 3 5.264 3H2.5zM14 7H2v5.5a.5.5 0 0 0 .5.5h11a.5.5 0 0 0 .5-.5V7z"/>
+                                  </svg>
+                                Manage Forms
+                            </button>
+                        </div>
+                    </div>
+
                     <table class="table" style="table-layout: fixed;">
                         <thead>
                             <tr>
