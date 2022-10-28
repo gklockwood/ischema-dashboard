8525780ad3f65b0dbe162732149ed913d73c8424
Galen Lockwood <galenlockwood@gmail.com>
2022-10-28 13:04:24 -0400

    updates to clients list

--- src/views/therapist/Clientlist.vue
+++ src/views/therapist/Clientlist.vue
@@ -1,16 +1,15 @@
 <template>
     <div class="clientlist">
         <div class="container py-4">
-            <h1>Client List</h1>
+            <h3>Clients & Contacts List</h3>
             
-
             <div class="row my-3">
                 <div class="col-sm col-sm-6 py-3">
                     <div class="input-group">
                         <input class="form-control border-end-0 border rounded-pill" type="search" value="Search Clients"
                             id="example-search-input">
                         <span class="input-group-append">
-                            <button class="btn btn-outline-secondary bg-white border-bottom-0 border rounded-pill ms-n5"
+                            <button class="btn btn-outline-primary bg-white border-bottom-0 border rounded-pill ms-n5"
                                 type="button">
                                 <i class="fa fa-search"></i>
                             </button>
@@ -43,63 +42,17 @@
                 </div>
             </div>
 
-
-
             <div class="card p-3 my-3 shadow-sm" v-for="client in clientList" :key="client.id"
                 :therapist-name="client.first_name" :id="client.id">
                 <div class="row">
-                    <div class="col-sm-3">
-                        <div class="row">
-                            <div class="col-sm-12">
-                                <h5 class="fw-normal">Name</h5>
-                            </div>
-                        </div>
-                        <div class="row">
-                            <div class="col-sm-12">
-                                <router-link to="/therapist/clientdetail" class="text-decoration-none"> {{ client.first_name }} {{ client.last_name }}</router-link>
-                            </div>
-                        </div>
+                    <div class="col-sm">
+                        <h5 class="m-0"><router-link to="/therapist/clientdetail" class="text-decoration-none"> {{ client.first_name }} {{ client.last_name }}</router-link></h5>
                     </div>
-                    <div class="col-sm-3">
-                        <div class="row">
-                            <div class="col-sm-12">
-                                <h5 class="fw-normal">Contact</h5>
-                            </div>
-                        </div>
-                        <div class="row">
-                            <div class="col-sm-12">
-                                <a href="{{ client.client_number }}" class="text-decoration-none">{{ client.client_number }}</a> <span class="badge bg-light text-dark ms-1 shadow-sm">Mobile</span>
-                            </div>
-                            <div class="col-sm-12">
-                                <hr class="my-2">
-                                <a href="mailto:{{ client.client_email }}" class="text-decoration-none">{{ client.client_email }}</a> <a href="#" class="ps-2 text-decoration-none"> <div class="vr me-2"></div> <i class="fas fa-copy"></i></a>
-                            </div>
-                            <div class="col-sm-12">
-                                <hr class="my-2">
-                                <button type="button" class="btn btn-sm btn-light mt-2 shadow-sm"><i class="fas fa-comment-alt me-2"></i>Message</button>
-                            </div>
-                        </div>
-
+                    <div class="col-sm">
+                        <a href="{{ client.client_number }}" class="text-decoration-none">{{ client.client_number }}</a> <span class="badge bg-light text-dark ms-1 shadow-sm">Mobile</span>
                     </div>
-                    <div class="col-sm-6 d-flex justify-content-center">
-                        <div class="row">
-                            <div class="col-sm-12 d-flex">
-                                <div class="dropdown d-flex">
-                                    <button class="btn btn-light dropdown-toggle shadow-sm align-self-center" type="button" id="dropdownMenuButton1"
-                                        data-bs-toggle="dropdown" aria-expanded="false">
-                                        <i class="fas fa-cogs me-1"></i> Manage
-                                    </button>
-                                    <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
-                                        <li><a class="dropdown-item" href="#"><i class="fas fa-eye me-1"></i> Client Details</a></li>
-                                        <li><a class="dropdown-item" href="#"><i class="fas fa-pencil-alt me-1"></i> Edit Client Details</a></li>
-                                        <li><a class="dropdown-item" href="#"><i class="fas fa-puzzle-piece me-1"></i> Merge with Duplicate</a></li>
-                                        <li><a class="dropdown-item" href="#"><i class="fas fa-user-friends me-1"></i> Add Client to Couple</a></li>
-                                        <li><a class="dropdown-item text-danger" href="#"><i class="fas fa-user-slash me-1"></i> Make Inactive</a></li>
-                                    </ul>
-                                </div>
-                            </div>
-                        </div>
-
+                    <div class="col-sm">
+                        <a href="mailto:{{ client.client_email }}" class="text-decoration-none">{{ client.client_email }}</a> <a href="#" class="ps-2 text-decoration-none"> <i class="fas fa-copy"></i></a>
                     </div>
                 </div>
             </div>
